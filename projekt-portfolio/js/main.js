d12a8d3f4a48ba34697ab1869fc4c585e6ba5ae1
AnnaBodzak <anna.bodzak@interia.pl>
2018-01-29 16:08:35 +0100

    ‘init’

---
+++ projekt-portfolio/js/main.js
@@ -0,0 +1,15 @@
+//$('#main-nav').css({
+//        background : '#ffffff00',
+////    color: 'white'
+//    });
+//$('#main-nav .navbar-brand').css({
+//    color: 'white'
+//    });
+//$('.navbar-light .navbar-nav .nav-link').css({
+//    color: 'white',
+//    textTransform: 'uppercase',
+//    fontSize: 'bold'
+//    });
+
+
+
